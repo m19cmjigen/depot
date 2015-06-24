61408e14e0625a88fed7eab50b85681a8a91c34e
m19cmjigen <m19cmjigen@gmail.com>
2015-06-25 01:44:08 +0900

    add cart #5

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
+
 validates :title, :description, :image_url, presence:true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
@@ -7,4 +8,17 @@ validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png)\z}i,
   message: 'はGIF、JPG、PNG画 像 のURLで な け れ ば な り ま せ ん'
 }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, '品目が存在します')
+        return false
+      end
+    end
 end
